eaba6f49a6e6bde5851496750e38e0e72a5b6881
desny <trsadesny@gmail.com>
2026-02-01 14:34:35 +0530

    student management system

--- student/app/models.py
+++ student/app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -13,6 +14,7 @@ class Students(models.Model):
     email=models.EmailField()
     age=models.IntegerField()
     phone=models.IntegerField()
+    uname=models.ForeignKey(User,on_delete=models.CASCADE)
     cname=models.ForeignKey(Courses,on_delete=models.CASCADE)
 
     def __str__(self):
